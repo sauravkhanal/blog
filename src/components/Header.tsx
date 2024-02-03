dfcee4b3853b098b1346da06508f08619a63b470
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-03 21:57:10 +0545

    Add hover effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ function Header() {
     <div className="flex px-10  text-black dark:text-gray-100 ">
         <p className="text-3xl font-extrabold">BLOG</p>
         <nav className=" text-xl flex-grow flex justify-end gap-10 ">
-            <a href="#" className="hover:bg-gray-300 dark:hover:bg-gray-700 px-2 py-1 rounded-none">About</a>
-            <a href="#" className="hover:bg-gray-300 dark:hover:bg-gray-700 px-2 py-1 rounded-none">Categories</a>
-            <a href="#" className="hover:bg-gray-300 dark:hover:bg-gray-700 px-2 py-1 rounded-none">Item</a>
+            <a href="#" className=" px-2 py-1 rounded-none navbar-hover-effect ">About</a>
+            <a href="#" className="px-2 py-1 rounded-none navbar-hover-effect">Categories</a>
+            <a href="#" className="px-2 py-1 rounded-none navbar-hover-effect">Item</a>
         </nav>
     </div>
   )
