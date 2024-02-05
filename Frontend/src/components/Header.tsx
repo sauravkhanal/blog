aefb6cef166c826362ba46bd7fdd9def229841a9
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-05 23:47:41 +0545

    add browser router

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,14 +1,20 @@
+import { NavLink, useLocation } from "react-router-dom"
+
 function Header() {
-  return (
-    <div className="flex px-10  text-black dark:text-gray-100 ">
-        <p className="text-3xl font-extrabold">BLOG</p>
-        <nav className=" text-xl flex-grow flex justify-end gap-10 ">
-            <a href="#" className=" px-2 py-1 rounded-none navbar-hover-effect ">About</a>
-            <a href="#" className="px-2 py-1 rounded-none navbar-hover-effect">Categories</a>
-            <a href="#" className="px-2 py-1 rounded-none navbar-hover-effect">Item</a>
-        </nav>
-    </div>
-  )
+	const path = useLocation().pathname;
+	return (
+		<div className="flex px-10  text-black dark:text-gray-100 ">
+			<p className="text-3xl font-extrabold">BLOG</p>
+			<nav className=" text-xl flex-grow flex-row flex justify-end gap-10 ">
+				<NavLink to="/" className="px-2 py-1 rounded-none navbar-hover-effect ">Home</NavLink>
+				<NavLink to="/about" className="px-2 py-1 rounded-none navbar-hover-effect ">About</NavLink>
+				<NavLink to="/categories" className="px-2 py-1 rounded-none navbar-hover-effect ">
+					Categories
+				</NavLink>
+
+			</nav>
+		</div>
+	)
 }
 
 export default Header
