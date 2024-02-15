f17d2dbe505297a889a1b60da4880d3624cc04dd
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-15 22:48:01 +0545

    return error only when error is caught

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,9 +1,11 @@
-import ApiError from "./ApiError";
+import ApiError from "./ApiError.js";
 
 export default function asyncHandler(asyncFunction) {
     return (req, res, next) => {
         Promise.resolve(asyncFunction(req, res, next))
-            .catch(error => console.error("Async Handler error: ", error))
-        res.status(500).json(new ApiError(500, "An unexpected error occurred"))
+            .catch(error => {
+                console.error("Async Handler error: ", error);
+                res.status(500).json(new ApiError(500, "An unexpected error occurred"));
+            });
     };
 }
