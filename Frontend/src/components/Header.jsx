fa17db0098806fafe74d54b2cd72fa9ed8b378f8
sauravkhanal <sauravkhanal635@gmail.com>
2024-02-17 13:58:44 +0545

    add name to logo and change home -> posts

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
 	const { isLoggedIn, logout } = useAuth();
 	return (
 		<nav className="flex px-10  text-black dark:text-gray-100 ">
-			<p className="text-3xl font-extrabold">BLOG</p>
+			<p className="text-3xl font-extrabold">Saurav's BLOG</p>
 			<nav className=" text-xl flex-grow flex-row flex justify-end gap-10 ">
-				<NavLink to="/" className="px-2 py-1 rounded-none navbar-hover-effect ">Home</NavLink>
+				<NavLink to="/" className="px-2 py-1 rounded-none navbar-hover-effect ">Posts</NavLink>
 				<NavLink to="/about" className="px-2 py-1 rounded-none navbar-hover-effect ">About</NavLink>
 				<NavLink to="/categories" className="px-2 py-1 rounded-none navbar-hover-effect ">Categories</NavLink>
 				{isLoggedIn() ?
