8b46ab549aec4dabc8a081c5414cb7116d3d0e26
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-13 17:50:15 +0545

    remove cookie parser

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,27 @@
 import express from "express"
-import cookieParser from "cookie-parser"
+// import cookieParser from "cookie-parser"
 import cors from "cors"
 
 const app = express();
 app.on("error", error => console.log(`Error occurred on creating express app: ${error}`))
 
+// app.use(cookieParser())//parse cookies(access token)
 
 //middlewares
-app.use(cors())
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//     credentials: true
+//   }));  
+app.use(cors());  
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  
 app.use(express.json());//parse JSON data
 app.use(express.urlencoded({ extended: true })); //parse form data 
 app.use(express.static("public")) //store some assets in public
-app.use(cookieParser())//parse cookies(access token)
 
 
 //routes
