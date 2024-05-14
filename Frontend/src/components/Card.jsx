8b32ed111d61570abe45048cabbd7e4f608728f5
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 09:25:05 +0545

    change aspect ratio of image

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card({ imageURL, category, title,slug, body, createdAt, minutesToRead }
   }
   return (
     <div className="group p-2 hover:scale-95 transition bg-white hover:bg-gray-200 hover:dark:bg-zinc-700 dark:bg-zinc-800 text-gray-500 max-w-sm font-semibold text-sm space-y-2 cursor-pointer font-nunito flex flex-col shadow-lg" onClick={handleClick}>
-      <img src={imageURL || placeholderImage} alt={title} className="object-contain p-0 " />
+      <img src={imageURL || placeholderImage} alt={title} className="object-cover p-0 aspect-[3/2]" />
       <p className="">{category.toUpperCase()}</p>
       <h2 className="text-gray-900 dark:text-gray-200 text-xl font-bold ">{title}</h2>
       <p className="font-normal text-justify overflow-ellipsis flex-grow line-clamp-2 text-base" dangerouslySetInnerHTML={{__html:body}}></p>
