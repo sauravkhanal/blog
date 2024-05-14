3dd0f7ccc128e0cd30e758196150cd5abf324d8f
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 17:46:02 +0545

    correct api endpoint

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // API_ENDPOINT: "http://localhost:8000/api/v1",
-    API_ENDPOINT: "https://blog.khanalsaurav.com.np/api/v1",
+    API_ENDPOINT: "https://api.blog.khanalsaurav.com.np/api/v1",
 }
 
 export default config
