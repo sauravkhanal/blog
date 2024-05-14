0b4ab2e7991ca40c83fd32e0d95ff6770fff0bc5
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 14:35:26 +0545

    provide delete post route, fix get post from slug

--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -1,14 +1,17 @@
 import express from "express"
-import { createPost, getPost, getPostFromId, getPostFromSlug } from "../controllers/post.controller.js";
+import { createPost, deletePostFromId, getPost, getPostFromId, getPostFromSlug } from "../controllers/post.controller.js";
 import upload from "../middlewares/multer.middleware.js";
-import {verifyAccessToken} from "../middlewares/auth.middleware.js"
+import { verifyAccessToken } from "../middlewares/auth.middleware.js"
 
 const postRouter = express.Router();
 
-postRouter.route("/").get(getPost)
 postRouter.route("/").post(verifyAccessToken, upload.single("coverImage"), createPost)
 postRouter.route("/me").get(verifyAccessToken, getPostFromId)
 postRouter.route("/:slug").get(getPostFromSlug)
+postRouter.route("/").get(getPost)
+postRouter.route("/:id").delete(verifyAccessToken, deletePostFromId);
+
+postRouter.all("/", (req, res)=>res.status(404).json({message: "route not found"}))
 
 
 
