c735c9e733d083a8490b77df39c9dddd6df2a793
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 16:52:45 +0545

    change endpoint

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: "http://localhost:8000/api/v1",
+    // API_ENDPOINT: "http://localhost:8000/api/v1",
     API_ENDPOINT: "https://blog.khanalsaurav.com.np/api/v1",
 }
 
