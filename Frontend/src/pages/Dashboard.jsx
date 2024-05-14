8ed2bcfcbea23bb812ee324fd101926a815980a0
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 13:47:39 +0545

    implement basic dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import fetchMyBlogs from "../api/fetchMyBlogs";
+import MyPostCard from "../components/MyPostCard";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+	const [posts, setPosts] = useState();
+	useEffect(() => {
+		(async function () {
+			const myPosts = await fetchMyBlogs();
+			setPosts(myPosts);
+		})();
+	}, []);
+	return (
+		<>
+			{posts ? (
+				<div className=" rounded-md  max-w-2xl px-2 py-5">
+					<p className="my-5 text-2xl font-bold">My Posts</p>
+					{
+						posts.slice().reverse().map((value, index) => <MyPostCard post={value} key={value.slug} />)
+					}
+				</div>
+			) : (
+				<p>You've not created any post</p>
+			)}
+		</>
+	);
 }
