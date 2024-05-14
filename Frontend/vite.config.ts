83fc4513fabbdefddadb77314fab4dc98a72b010
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 09:24:11 +0545

    remove proxy

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,13 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "../docs"
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.blog.khanalsaurav.com.np",
-        secure: false,
-      }
-    }
   }
 })
