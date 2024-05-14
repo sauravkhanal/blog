c56838257801e9915685a0f9e1aa104c5e5c6931
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-14 16:43:55 +0545

    add deletion logic

--- Frontend/src/components/MyPostCard/index.jsx
+++ Frontend/src/components/MyPostCard/index.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Button from "../Button"
+import ConfirmationModal from "../ConfirmationModal";
+import deletePostById from "../../api/deletePostById";
 function stripTags(html) {
 	return html.replace(/<[^>]*>?/gm, '');
 }
 
 export default function MyPostCard({ post }) {
-	const { title, createdAt, updatedAt, body } = post;
+	const [modalVisible, setModalVisible] = useState(false)
+	// const [isBeingDeleted, setIsBeingDeleted] = useState(false)
+	const [isDeleted, setIsDeleted] = useState(false)
+	const { title, createdAt, updatedAt, body, _id } = post;
 	const createdDate = new Date(createdAt)
-	// const updatedDate = new Date(updatedAt)
-	// console.log(post)
+
+	const handleDelete = async () => {
+		const result = await deletePostById(_id)
+		if  (result) setIsDeleted(true)
+		//else notify couldn't delete
+	}
+
 	return (
+		!isDeleted &&
 		<div className="flex flex-col p-2 md:px-4 bg-gray-300 dark:bg-gray-900 rounded-md my-2">
 			<div className="flex flex-col md:flex-row h-full  gap-5">
 				<span className="flex-grow flex flex-col justify-center items-center">
@@ -21,9 +33,10 @@ export default function MyPostCard({ post }) {
 				</span>
 				<div className="flex md:flex-col gap-2 flex-grow self-center h-full">
 					<Button className="bg-yellow-600">Edit</Button>
-					<Button className="bg-red-700">Delete</Button>
+					<Button className="bg-red-700" onClick={() => setModalVisible(true)}>Delete</Button>
 				</div>
 			</div>
+			<ConfirmationModal modalVisible={modalVisible} setModalVisible={setModalVisible} destructive={false} text={"Are you sure you want to delete the post?"} onConfirm={handleDelete} />
 		</div>
 	);
 }
